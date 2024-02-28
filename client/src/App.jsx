2b9e583bfe60f48f50d3541d48b32ba0b58e188b
injili <nyarekigospel@gmail.com>
2024-02-28 16:05:50 +0300

    the routes are on

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,20 +8,36 @@ import './App.css';
 import { Home } from "./pages/Home";
 import { Product } from "./pages/Product";
 import { Products } from "./pages/Products";
-
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+const Layout = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
 const router = createBrowserRouter([
   {
     path:'/',
-    element:<span><Home/></span>
-  },
-  {
-    path:'/products/:id',
-    element:<span><Products/></span>
-  },
-  {
-    path:'/product/:id',
-    element:<span><Product/></span>
-  },
+    element:<Layout/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      },
+      {
+        path:'/products/:id',
+        element:<Products/>
+      },
+      {
+        path:'/product/:id',
+        element:<Product/>
+      }
+    ]
+  }
 ])
 
 function App() {
